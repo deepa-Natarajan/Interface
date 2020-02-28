9fdc193af9de7756fc182c3ef42867eb162cf78c
deepa-Natarajan <53449985+deepa-Natarajan@users.noreply.github.com>
2020-02-28 02:21:09 -0600

    interface

---
+++ pack/src/P1/Demo.java
@@ -0,0 +1,17 @@
+package P1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		Protection ob1=new Protection();
+		Derived ob2=new Derived();
+		SamePackage ob3=new SamePackage();
+		
+		//ob1.toString();
+		//ob2.toString();
+			ob3.toString();
+	}
+
+}
